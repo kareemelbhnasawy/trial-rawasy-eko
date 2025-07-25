76dc7f5bc9658ef030a333462b6403b6b9508947
kgamal <kgamal@sumerge.com>
2025-07-26 01:39:25 +0300

    modified till it is almost rawasy acceptable, notes: make it egp, located in egypt, etc..
    (cherry picked from commit ca6f5b66b32c9297daefbd63419d39ed4e0dc86a)

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -6,10 +6,11 @@ function NavItem() {
             <nav>
                 <ul className="parent-nav">
                     <li className="parent has-dropdown">
-                        <a className="nav-link" href="#">
+                        {/* <a className="nav-link" href="#"> */}
+                        <a className="nav-link" href="/">
                             Home
                         </a>
-                        <ul className="submenu">
+                        {/* <ul className="submenu">
                             <li>
                                 <a className="sub-b" href="/">
                                     Home One
@@ -35,7 +36,7 @@ function NavItem() {
                                     Home Five
                                 </a>
                             </li>
-                        </ul>
+                        </ul> */}
                     </li>
                     <li className="parent">
                         <a href="/about">About</a>
@@ -194,9 +195,10 @@ function NavItem() {
                             <li>
                                 <a className="sub-b" href="/vendor-list">
                                     Vendor List
+                                     <span className="badge">New</span>
                                 </a>
                             </li>
-                            <li>
+                            {/* <li>
                                 <a className="sub-b" href="/vendor-grid">
                                     Vendor Grid
                                 </a>
@@ -205,10 +207,10 @@ function NavItem() {
                                 <a className="sub-b" href="/vendor-details">
                                     Vendor Details
                                 </a>
-                            </li>
+                            </li> */}
                         </ul>
                     </li>
-                    <li className="parent has-dropdown">
+                    {/* <li className="parent has-dropdown">
                         <a className="nav-link" href="#">
                             Pages
                         </a>
@@ -275,8 +277,8 @@ function NavItem() {
                                 </a>
                             </li>
                         </ul>
-                    </li>
-                    <li className="parent has-dropdown">
+                    </li> */}
+                    {/* <li className="parent has-dropdown">
                         <a className="nav-link" href="#">
                             Blog
                         </a>
@@ -309,7 +311,7 @@ function NavItem() {
                                 </a>
                             </li>
                         </ul>
-                    </li>
+                    </li> */}
                     <li className="parent">
                         <a href="/dashboard">
                             Dashboard
